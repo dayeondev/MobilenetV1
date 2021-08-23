7e01124a14c990c4cc104dd8c01a42d97553c3a8
Dayeon Oh <dayeon.dev@gmail.com>
2021-08-23 12:18:40 +0900

    Edit training code

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ import numpy as np
 from PIL import Image
 import time
 import matplotlib.pyplot as plt
+from torch import nn
 
 PATH = './flower_mobilenet.pth'
 
@@ -43,7 +44,7 @@ classes = ('daisy', 'dandelion', 'roses', 'sunflowers','tulips')
 
 DATA_PATH_TRAINING_LIST = glob('./../data/flower_split/train/*/*.jpg')
 DATA_PATH_TESTING_LIST = glob('./../data/flower_split/test/*/*.jpg')
-batch_size = 32
+batch_size = 16
 train_loader = DataLoader(
     datasets.FlowersDataset(
         DATA_PATH_TRAINING_LIST, 
@@ -83,6 +84,7 @@ summary(model, (3, 224, 224), device=device.type)
 loss_func = losses.crossEntropyLoss()
 
 
+
 #%%###############################
 # define optimizer and scheduler #
 ##################################
@@ -90,162 +92,104 @@ opt = optim.Adam(model.parameters(), lr=0.01)
 
 lr_scheduler = ReduceLROnPlateau(opt, mode='min', factor=0.1, patience=10)
 
-
-
 #%%#########
 # training #
 ############
-# get current lr
-def get_lr(opt):    
-    for param_group in opt.param_groups:
-        return param_group['lr']
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
 
+def show_accuracy(model, history, data_loader):
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
 
-# calculate the metric per mini-batch
-def metric_batch(output, target):
-    pred = output.argmax(1, keepdim=True)
-    corrects = pred.eq(target.view_as(pred)).sum().item()
-    return corrects
+    with torch.no_grad():
+        for data in data_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predictions = torch.max(outputs, 1)
+            # Check correct prediction
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
 
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        history[classname].append(accuracy)
 
-# calculate the loss per mini-batch
-def loss_batch(loss_func, output, target, opt=None):
-    loss_b = loss_func(output, target)
-    metric_b = metric_batch(output, target)
-
-    if opt is not None:
-        opt.zero_grad()
-        loss_b.backward()
-        opt.step()
+def training(model, epoch):
     
-    return loss_b.item(), metric_b
+    loss_history = {'train': []}
+    test_accuracy_history = {'daisy': [], 'dandelion': [], 'roses': [], 'sunflowers': [],'tulips': []}
+    valid_accuracy_history = {'daisy': [], 'dandelion': [], 'roses': [], 'sunflowers': [],'tulips': []}
 
+    for epoch in range(epoch):
+        loss_sum = 0
+        loss_count = 0
+        for i, data in enumerate(train_loader, 0):
+            
+            inputs, labels = data[0].to(device), data[1].to(device)
 
-# calculate the loss per epochs
-def loss_epoch(model, loss_func, dataset_dl, sanity_check=False, opt=None):
-    running_loss = 0.0
-    running_metric = 0.0
-    len_data = len(dataset_dl.dataset)
+            optimizer.zero_grad()
 
-    for xb, yb in dataset_dl:
-        xb = xb.to(device)
-        yb = yb.to(device)
-        output = model(xb)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        loss_b, metric_b = loss_batch(loss_func, output, yb, opt)
+            # print(loss.item())
+            loss_sum += loss.item()
+            loss_count += 1
+            # loss_history['train'].append(loss.item())
 
-        running_loss += loss_b
+        loss_history['train'].append(loss_sum/loss_count)
+        print("epoch: {:3d}, {:.2f} ".format(epoch,  loss_sum/loss_count))
+        show_accuracy(model, test_accuracy_history, train_loader)
+        show_accuracy(model, valid_accuracy_history, valid_loader)
         
-        if metric_b is not None:
-            running_metric += metric_b
-
-        if sanity_check is True:
-            break
-
-    loss = running_loss / len_data
-    metric = running_metric / len_data
-    return loss, metric
 
 
-# function to start training
-def train_val(model, params):
-    num_epochs=params['num_epochs']
-    loss_func=params['loss_func']
-    opt=params['optimizer']
-    train_dl=params['train_dl']
-    val_dl=params['val_dl']
-    sanity_check=params['sanity_check']
-    lr_scheduler=params['lr_scheduler']
-    path2weights=params['path2weights']
-
-    loss_history = {'train': [], 'val': []}
-    metric_history = {'train': [], 'val': []}
-
-    best_loss = float('inf')
-    best_model_wts = copy.deepcopy(model.state_dict())
-    start_time = time.time()
-
-    for epoch in range(num_epochs):
-        current_lr = get_lr(opt)
-        print('Epoch {}/{}, current lr= {}'.format(epoch, num_epochs-1, current_lr))
-
-        model.train()
-        train_loss, train_metric = loss_epoch(model, loss_func, train_dl, sanity_check, opt)
-        loss_history['train'].append(train_loss)
-        metric_history['train'].append(train_metric)
-
-        model.eval()
-        with torch.no_grad():
-            val_loss, val_metric = loss_epoch(model, loss_func, val_dl, sanity_check)
-        loss_history['val'].append(val_loss)
-        metric_history['val'].append(val_metric)
-
-        if val_loss < best_loss:
-            best_loss = val_loss
-            best_model_wts = copy.deepcopy(model.state_dict())
-            torch.save(model.state_dict(), path2weights)
-            print('Copied best model weights!')
-
-        lr_scheduler.step(val_loss)
-        if current_lr != get_lr(opt):
-            print('Loading best model weights!')
-            model.load_state_dict(best_model_wts)
-
-        print('train loss: %.6f, val loss: %.6f, accuracy: %.2f, time: %.4f min' %(train_loss, val_loss, 100*val_metric, (time.time()-start_time)/60))
-        print('-'*10)
-
-    model.load_state_dict(best_model_wts)
-    torch.save(model.state_dict(), PATH)
-    return model, loss_history, metric_history
+        torch.save(model.state_dict(), PATH)
+    return model, loss_history, test_accuracy_history, valid_accuracy_history
 
 # %%
-# define the training parameters
-params_train = {
-    'num_epochs':30,
-    'optimizer':opt,
-    'loss_func':loss_func,
-    'train_dl':train_loader,
-    'val_dl':valid_loader,
-    'sanity_check':False,
-    'lr_scheduler':lr_scheduler,
-    'path2weights':'./models/weights.pt',
-}
-
-# check the directory to save weights.pt
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSerror:
-        print('Error')
-createFolder('./models')
-
-
-# %%
-model, loss_hist, metric_hist = train_val(model, params_train)
-
+epoch=100
+_, loss_hist, train_accu_hist, valid_accu_test = training(model, epoch)
 # %%
 # train-val progress
-num_epochs = params_train['num_epochs']
+# num_epochs = params_train['num_epochs']
+num_epochs = len(loss_hist['train'])
 
 # plot loss progress
-plt.title('Train-Val Loss')
+plt.title('Train Loss')
 plt.plot(range(1, num_epochs+1), loss_hist['train'], label='train')
-plt.plot(range(1, num_epochs+1), loss_hist['val'], label='val')
-plt.ylabel('Loss')
-plt.xlabel('Training Epochs')
+# plt.plot(range(1, num_epochs+1), loss_hist['val'], label='val')
+plt.ylabel('loss avg')
+plt.xlabel('epoch')
 plt.legend()
 plt.show()
 
-# plot accuracy progress
-plt.title('Train-Val Accuracy')
-plt.plot(range(1, num_epochs+1), metric_hist['train'], label='train')
-plt.plot(range(1, num_epochs+1), metric_hist['val'], label='val')
-plt.ylabel('Accuracy')
-plt.xlabel('Training Epochs')
+plt.title('Train Accuracy')
+plt.plot(range(1, num_epochs+1), train_accu_hist['daisy'], label='daisy')
+plt.plot(range(1, num_epochs+1), train_accu_hist['dandelion'], label='dandelion')
+plt.plot(range(1, num_epochs+1), train_accu_hist['roses'], label='roses')
+plt.plot(range(1, num_epochs+1), train_accu_hist['sunflowers'], label='sunflowers')
+plt.plot(range(1, num_epochs+1), train_accu_hist['tulips'], label='tulips')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
 plt.legend()
 plt.show()
 
+plt.title('Valid Accuracy')
+plt.plot(range(1, num_epochs+1), valid_accu_test['daisy'], label='daisy')
+plt.plot(range(1, num_epochs+1), valid_accu_test['dandelion'], label='dandelion')
+plt.plot(range(1, num_epochs+1), valid_accu_test['roses'], label='roses')
+plt.plot(range(1, num_epochs+1), valid_accu_test['sunflowers'], label='sunflowers')
+plt.plot(range(1, num_epochs+1), valid_accu_test['tulips'], label='tulips')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
 
 #%%############
 # show result #
